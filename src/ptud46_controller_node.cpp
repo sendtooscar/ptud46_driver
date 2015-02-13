fff92105401a1cbab5fb54b1b82ef8dc99d79877
Himanshu Ragtah <himanshuragtah1@gmail.com>
2015-02-13 11:43:06 -0330

    Update ptud46_controller_node.cpp
    do i really need that sendCmd function for the laser scanner? trying to find a efficient way to tackle this.
    and do we actually define the max_ang and min_ang as 30 and -30 or does it automatically take that given that's the max our laser scanner can go?
    I have to change the variable names too.

--- src/ptud46_controller_node.cpp
+++ src/ptud46_controller_node.cpp
@@ -10,7 +10,11 @@ TODO 4) generate point cloud from the laser scans and publsh
 #include "std_msgs/String.h"
 #include "geometry_msgs/Vector3.h"
 #include <tf/transform_broadcaster.h>
+#include "sensor_msgs/LaserScan.h"
 
+#include "hokuyo_node/HokuyoConfig.h"
+
+#include "hokuyo_node/hokuyo.h"
 #include <stdio.h>
 
 bool ptuReady;
@@ -23,8 +27,96 @@ void ptureadyCallback(const std_msgs::String::ConstPtr& msg)
   	ptuReady=false;
   }
 }
+/////////////////////////////////////////////
+int hokuyo::Laser::pollScan(hokuyo::LaserScan& scan, double min_ang, double max_ang, int cluster, int timeout)
+{
+  if (!portOpen())
+    HOKUYO_EXCEPT(hokuyo::Exception, "Port not open.");
+
+  int status;
+
+  // Always clear ranges/intensities so we can return easily in case of erro
+  scan.ranges.clear();
+  scan.intensities.clear();
+
+  // clustering of 0 and 1 are actually the same
+  if (cluster == 0)
+    cluster = 1;
+  
+  int min_i = (int)(afrt_ + min_ang*ares_/(2.0*M_PI));
+  int max_i = (int)(afrt_ + max_ang*ares_/(2.0*M_PI));
+  
+  char cmdbuf[MAX_CMD_LEN];
+  
+  sprintf(cmdbuf,"GD%.4d%.4d%.2d", min_i, max_i, cluster);
+  
+  status = sendCmd(cmdbuf, timeout);
+  
+  scan.system_time_stamp = timeHelper() + offset_;
+  
+  if (status != 0)
+    return status;
+  
+  // Populate configuration
+  scan.config.min_angle  =  (min_i - afrt_) * (2.0*M_PI)/(ares_);
+  scan.config.max_angle  =  (max_i - afrt_) * (2.0*M_PI)/(ares_);
+  scan.config.ang_increment =  cluster*(2.0*M_PI)/(ares_);
+  scan.config.time_increment = (60.0)/(double)(rate_ * ares_);
+  scan.config.scan_time = 0.0;
+  scan.config.min_range  =  dmin_ / 1000.0;
+  scan.config.max_range  =  dmax_ / 1000.0;
+  
+  readData(scan, false, timeout);
+  
+  long long inc = (long long)(min_i * scan.config.time_increment * 1000000000);
+  
+  scan.system_time_stamp += inc;
+  scan.self_time_stamp += inc;
+  
+  return 0;
+}
+///////////////////////////////////////////////////////////////////////////////
+int hokuyo::Laser::sendCmd(const char* cmd, int timeout)
+{
+if (!portOpen())
+    HOKUYO_EXCEPT(hokuyo::Exception, "Port not open.");
+
+  char buf[100]; 
+
+  laserWrite(cmd);
+  laserWrite("\n");
+
+  laserReadlineAfter(buf, 100, cmd, timeout);
+  laserReadline(buf,100,timeout);
+
+  if (!checkSum(buf,4))
+    HOKUYO_EXCEPT(hokuyo::CorruptedDataException, "Checksum failed on status code.");
+
+  buf[2] = 0;
+  
+  if (buf[0] - '0' >= 0 && buf[0] - '0' <= 9 && buf[1] - '0' >= 0 && buf[1] - '0' <= 9)
+    return (buf[0] - '0')*10 + (buf[1] - '0');
+  else
+    HOKUYO_EXCEPT(hokuyo::Exception, "Hokuyo error code returned. Cmd: %s --  Error: %s", cmd, buf);
+}
 
 
+///////////////////////////////////////////////////////////////////////////////
+void cmdpose_callback(const geometry_msgs::Vector3 cmdpose_msg){
+	double old_pan_cmd = pan_cmd;
+	double old_tilt_cmd = tilt_cmd;
+	pan_cmd=cmdpose_msg.x;
+	tilt_cmd=cmdpose_msg.y;
+	//std::cout<<pan_ang<<std::endl;
+	if(pan_cmd==old_pan_cmd && tilt_cmd==old_tilt_cmd){
+		new_cmd=false;
+	}
+	else{
+		new_cmd=true;
+	}
+}
+
+///////////////////////////////////////////////////////
 int main(int argc, char **argv){
 
 	// Initialize ROS
@@ -43,6 +135,15 @@ int main(int argc, char **argv){
 	bool fwdt=true;
 	while (ros::ok())
 	{   
+		   // Set up polling callback
+    int hz;
+    ros::param::param<int>("~hz", hz, PTU_DEFAULT_HZ);
+    ros::Timer spin_timer = n.createTimer(ros::Duration(1 / hz),
+        &hokoyu_driver::Node::spinCallback, &ptu_node);
+
+    // Spin until there's a problem or we're in shutdown
+    ros::spin();
+    
 	  if(ptuReady){
 	    geometry_msgs::Vector3 msg;
 	    msg.x = 1000;
